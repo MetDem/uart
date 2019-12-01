c5c99e8eb921e203ec648dfb33b9313dfe366de2
Suleyman Taha <tahasecen@gmail.com>
2019-12-02 01:15:50 +0300

    Update unpacker.vhd

--- uart/unpacker.vhd
+++ uart/unpacker.vhd
@@ -21,10 +21,10 @@ end entity unpacker;
 architecture rtl of unpacker is
     type state_type is (rst, check_src, check_dst, check_ident, decryp, check_sum, send_data);
     signal state : state_type := rst;
-    signal s_buff_payload:  std_logic_vector(127 downto 0) := (others=>'0');
-    signal s_buff_checksum: std_logic_vector(D downto 0) := (others => '0');
-    signal s_encryp_data : std_logic_vector(127 downto 0) := (others=> '0');
+
     signal s_key : std_logic_vector(7 downto 0) := (others=> '1');
+    signal s_buff_payload: std_logic_vector(127 downto 0) := (others => '0');
+
 
     signal s_cnt : unsigned(3 downto 0) := "0000";
     signal s_cntdone : unsigned(3 downto 0) := "1111" ;
@@ -47,7 +47,7 @@ begin
     begin 
     if rising_edge(i_clk) then
         if cntrst ='1' then
-            cnt<= to_unsigned(((M-1),32);
+            cnt<= to_unsigned(M-1,32);
             cntdone<='0';
         else 
             if cnt=0 then
@@ -70,21 +70,19 @@ begin
         state <= rst;
         cntrst <= '0';
         o_ready <= '0';
-        o_packer <= x"00";
+        o_unpacker <= x"00";
 
         case state is 
 
             when rst =>
                 cntrst <= '1';
-                s_buff_payload <= (others=>'0');
-                s_buffer <= (others=>'0');
-                s_encryp_data <= (others=>'0');
+                s_buff_payload <= (others => '0');
 
 
                 if (i_rx_ready = '1') then 
                     cntrst <= '0';
                     s_src_addr_ready <= '1';
-                    state <= src;
+                    state <= check_src;
                 end if;
 
             when check_src => 
@@ -103,13 +101,13 @@ begin
                     if(i_rx = OUR_ADDRESS) then
                         s_dst_addr_ready <= '0';
                         s_identifier_ready <= '1';
-                        state <= ident;
+                        state <= check_ident;
                     else
                         state <= rst;
                     end if;
                 end if;
             
-            when ident =>
+            when check_ident =>
                 if(s_identifier_ready = '1' and i_rx_ready = '1') then
                     if(i_rx = IDENTIFIER) then
                         s_identifier_ready <= '0';
@@ -117,6 +115,7 @@ begin
                         state <= decryp;
                     else
                         state <= decryp;
+                    end if;
                 end if;
             
             when decryp => 
@@ -124,18 +123,18 @@ begin
                         s_buff_payload(127 downto 120) <= s_key xor i_rx;
                         if(s_cnt = s_cntdone) then
                             s_checksum_ready <= '1';
-                            state <= check;
+                            state <= check_sum;
                             s_cnt <= "0000";
-                            s_payload_ready <= '0';
+                            s_decryp_ready <= '0';
                         else
                             s_cnt <= s_cnt + 1;
                             s_buff_payload <= s_buff_payload(119 downto 0) & s_buff_payload(127 downto 120);
-                            state <= payload;
-                            s_payload_ready <= '1';
+                            state <= decryp;
+                            s_decryp_ready <= '1';
                         end if;    
                 end if;
 
-                when check => 
+                when check_sum => 
                 if(s_checksum_ready = '1' and i_rx_ready = '1') then
                     if(i_rx = CHECKSUM) then
                         s_send_data_ready <= '1';
@@ -149,14 +148,14 @@ begin
 
             when send_data => 
                 if(s_send_data_ready = '1' and cntdone = '1') then
-                    o_packer <= s_buff_payload(7 downto 0);
+                    o_unpacker <= s_buff_payload(7 downto 0);
                     if(s_cnt = s_cntdone) then
                         s_send_data_ready <= '0';
                         s_cnt <= "0000";
                         state <= rst;
                         o_ready <= '1';
                     else
-                        s_buffer <= s_buff_payload(7 downto 0) & s_buff_payload(127 downto 8);
+                        s_buff_payload <= s_buff_payload(7 downto 0) & s_buff_payload(127 downto 8);
                         s_cnt <= s_cnt + 1;
                         s_send_data_ready <= '1';
                         state <= send_data;
